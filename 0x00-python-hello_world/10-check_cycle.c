523b65a91e4008dbc21fe19255e0fc531d7f12f4
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2023-10-02 15:37:08 -0700

    circular

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list is circular
+ * @list: node of list
+ *
+ * Return: 1 if circular, 0 otherwise
+ *
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+
+	do {
+		temp = temp->next;
+		if (temp == list)
+			return (1);
+	} while (temp);
+
+	return (0);
+}
