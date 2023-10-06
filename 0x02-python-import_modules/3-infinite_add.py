fa8e6ac8835edd52b00bd460c2bc8b7c421542a6
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2023-10-05 19:00:13 -0700

    task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print(0)
+    else:
+        infin_add = 0
+        for i, j  in enumerate(sys.argv):
+            if i == 0:
+                continue
+            infin_add += int(j)
+        print(infin_add)
