41be1edeecd578e5881383035ce31dc647e3d17a
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2023-10-05 18:10:25 -0700

    task2

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as calculator
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, calculator.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calculator.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calculator.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calculator.div(a, b)))
