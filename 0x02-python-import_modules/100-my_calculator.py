c8eb6aa1b311f079b53b236f5ed37c201d636aee
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2023-10-05 19:55:31 -0700

    task100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import calculator_1 as calculator
+    if len(sys.argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    if sys.argv[2] not in '+-*/':
+        print('Unknown operator. Available operators: +, -, * and /')
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, calculator.add(a, b)))
+    elif operator == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, calculator.sub(a, b)))
+    elif operator == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, calculator.mul(a, b)))
+    elif operator == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, calculator.div(a, b)))
