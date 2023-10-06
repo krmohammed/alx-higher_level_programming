f220e757957896e74274719a5bead010ca8cbb2f
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2023-10-05 19:28:41 -0700

    task4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def __init__():
+    return True
+
+
+def my_secret_santa():
+    return True
+
+
+def print_school():
+    return True
+
+
+def print_hidden():
+    return True
+
+print(my_secret_santa.__name__)
+print(print_school.__name__)
+print(print_hidden.__name__)
