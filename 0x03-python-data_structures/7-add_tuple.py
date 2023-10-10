7a86f1c9bc172c050bc880c99fb1b0514521d66a
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2023-10-09 20:07:09 -0700

    tuple world

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a, b = tuple_a
-    c, d = tuple_b
-
+    a, b, c, d = 0, 0, 0, 0
+    if len(tuple_a) == 1:
+        a = tuple_a[0]
+    if len(tuple_a) > 1:
+        a = tuple_a[0]
+        b = tuple_a[1]
+    if len(tuple_b) == 1:
+        c = tuple_b[0]
+    if len(tuple_b) > 1:
+        c = tuple_b[0]
+        d = tuple_b[1]
     return (a + c, b + d)
