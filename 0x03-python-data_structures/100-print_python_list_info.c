a2f13d3974a102d5e1c484d2e59b325161e8c0b7
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2023-10-09 19:48:29 -0700

    cpython is fun

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+	int size, allocated, k;
+	PyObject *item;
+
+	if (!PyList_Check(p))
+		return;
+
+	size = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n",  allocated);
+
+	for (k = 0; k < PyList_Size(p); k++)
+	{
+		item = PyList_GetItem(p, k);
+		printf("Element %d: %s\n", k, Py_TYPE(item)->tp_name);
+	}
+}
