edaeb3ee34b0edb54cb3739179f7959a316c5618
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2023-10-09 17:11:26 -0700

    taskfix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
             if j == len(i) - 1:
-                print("{}".format(i[j]))
+                print("{:d}".format(i[j]))
             else:
-                print("{}".format(i[j]), end=" ")
-    print()
+                print("{:d}".format(i[j]), end=" ")
