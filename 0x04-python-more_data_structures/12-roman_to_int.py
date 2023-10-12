d802045ceef023db48a1ee873d8930e6f37f231c
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2023-10-11 19:31:09 -0700

    hot fixx

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if isinstance(roman_string, str) or roman_string is not None:
-        r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,\
-                'C': 100, 'D': 500, 'M': 1000}
+        r_dict = {
+                'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000
+                }
         value = 0
         for i in range(len(roman_string)):
             if i < len(roman_string) - 1:
