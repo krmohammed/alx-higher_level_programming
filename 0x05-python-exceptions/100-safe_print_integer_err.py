62491bb4b0c900ba058cb3599428d731971c63d4
mohrahman <mohrahman558@gmail.com>
2023-10-23 20:32:03 +0000

    two advanced tasks

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err_msg:
+        sys.stderr.write("Exception: {}\n".format(err_msg))
+        return False
+    else:
+        return True
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (IndexError, TypeError, ValueError, ZeroDivisionError) as err_msg:
+        sys.stderr.write("Exception: {}\n".format(err_msg))
