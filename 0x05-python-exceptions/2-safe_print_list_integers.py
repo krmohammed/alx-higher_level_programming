870fb137b76d1ec7e1eea7ed9636ef42d3ed6ef4
mohrahman <mohrahman558@gmail.com>
2023-10-23 17:51:47 +0000

    fixing bare excepts

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             int_len += 1
-        except:
+        except (TypeError, ValueError, IndexError):
             continue
     print()
     return int_len
