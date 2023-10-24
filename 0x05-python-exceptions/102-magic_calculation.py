32c33fc773f5699459e45eaaad74afb1c8228759
mohrahman <mohrahman558@gmail.com>
2023-10-24 01:20:25 +0000

    bare exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except (ZeroDivisionError, TypeError, ValueError):
+        except (ZeroDivisionError, TypeError, ValueError) as err:
             result = b + a
             break
     return result
