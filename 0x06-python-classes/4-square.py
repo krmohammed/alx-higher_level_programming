f8de6bcb19e6031979cac45d2a9902e178944ba1
mohrahman <mohrahman558@gmail.com>
2023-10-25 01:28:27 +0000

    task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,7 +13,7 @@ class Square:
     """
     def __init__(self, size=0):
         """
-        creates the size parameter with validation
+        creates the size parameter
 
         Args:
             size (int): size of square
@@ -22,12 +22,7 @@ class Square:
             TypeError: if size is not an int
             ValueError: if size is less than 0
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     @property
     def size(self):
