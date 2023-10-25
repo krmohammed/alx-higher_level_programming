3ed0b1d1ce16adf4ee24024108c39a9073aac145
mohrahman <mohrahman558@gmail.com>
2023-10-25 03:44:41 +0000

    task 103

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/bin/usr/python3
+"""Magic Class doing exactly as the Python bytecode
+"""
+
+
+class MagicClass:
+    """
+    MagicClass from a bytecode
+    """
+    def __init__(self, radius=0):
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """area of a circle"""
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """circumference of a circle"""
+        return 2 * math.pi * self.__radius
