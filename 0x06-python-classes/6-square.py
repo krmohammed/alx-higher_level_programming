f3c0e7ea34fb3ab9b28fd47dad6bf432f2be3b9b
mohrahman <mohrahman558@gmail.com>
2023-10-25 22:00:06 +0000

    hot fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -22,8 +22,8 @@ class Square:
             TypeError: if size is not an int
             ValueError: if size is less than 0
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
