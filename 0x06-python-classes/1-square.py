857b1ab7014917121108400ef94eb7012112aacb
mohrahman <mohrahman558@gmail.com>
2023-10-25 00:51:31 +0000

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 class Square:
-    """
+    """Square class initializes size attribute privately
+
+    Attributes:
+        no public attribute
     """
     def __init__(self, size):
+        """
+        creates the paramaters for each instance of this class
+        
+        Args:
+            size (int): size of square
+        """
         self.__size = size
