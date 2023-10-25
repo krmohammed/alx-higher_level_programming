6223a8106bdc7f6c7f494c86931898e163d51057
mohrahman <mohrahman558@gmail.com>
2023-10-25 03:19:22 +0000

    task 102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+Defines a square class
+"""
+
+
+class Square:
+    """
+    Square class initializes size attribute privately
+
+    Attributes:
+        no public attribute
+    """
+    def __init__(self, size=0):
+        """
+        creates the size parameter
+
+        Args:
+            size (int): size of square
+
+        Raises:
+            TypeError: if size is not an int
+            ValueError: if size is less than 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """size attribute's getter function
+
+        Returns:
+            the value of size attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Returns:
+            the current square area
+        """
+        return self.__size ** 2
+
+    def __eq__(self, comp):
+        if isinstance(comp, Square):
+            return self.area() == comp.area()
+        return False
+
+    def __ne__(self, comp):
+        if isinstance(comp, Square):
+            return self.area() != comp.area()
+        return False
+
+    def __gt__(self, comp):
+        if isinstance(comp, Square):
+            return self.area() > comp.area()
+        return False
+
+    def __ge__(self, comp):
+        if isinstance(comp, Square):
+            return self.area() >= comp.area()
+        return False
+
+    def __lt__(self, comp):
+        if isinstance(comp, Square):
+            return self.area() < comp.area()
+        return False
+
+    def __le__(self, comp):
+        if isinstance(comp, Square):
+            return self.area() <= comp.area()
+        return False
