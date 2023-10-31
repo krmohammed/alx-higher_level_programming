80a00ade8532f1349fc0915bbe776dec97232019
mohrahman <mohrahman558@gmail.com>
2023-10-31 02:33:03 +0000

    task 101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -8,16 +8,17 @@ N non-attacking queens on an N×N chessboard.
 
 import sys
 
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+    try:
+        N = int(sys.argv[1])
 
-N = sys.argv[1]
-if not isinstance(int(N), int):
-    print('N must be a number')
-    sys.exit(1)
-
-if int(N) < 4:
-    print('N must be at least 4')
-    sys.exit(1)
+        if N < 4:
+            print('N must be at least 4')
+            sys.exit(1)
+    except ValueError:
+        print('N must be an integer')
+        sys.exit(1)
