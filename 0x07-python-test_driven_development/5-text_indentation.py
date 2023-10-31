1875c8fa012c0b5a6931a599152ca6bda052f3f1
mohrahman <mohrahman558@gmail.com>
2023-10-31 17:59:41 +0000

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+provides one function, text_indentation()
+"""
+
+
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after ., ? and :
+
+    Args:
+    text (str): text to print
+    """
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    for i in range(len(text)):
+        if i == 0 and text[i] == ' ':
+            continue
+        if text[i] == ' ' and text[i - 1] in ['.', '?', ':']:
+            continue
+        print(text[i], end='')
+        if text[i] in ['.', '?', ':']:
+            print('\n')
