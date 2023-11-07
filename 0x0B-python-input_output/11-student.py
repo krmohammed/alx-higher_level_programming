06772172fcefa939bb233014616f7b6843134504
mohrahman <mohrahman558@gmail.com>
2023-11-07 22:21:58 +0000

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+provides the class Student
+"""
+
+
+class Student:
+    """
+    """
+    def __init__(self, first_name, last_name, age):
+        """initializes instance variables
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionary representation
+            of `self
+        """
+        if attrs is not None:
+            return {i: self.__dict__[i] for i in attrs if i in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of `self
+
+        Args:
+            json (dict): new attrs
+        """
+        self.__dict__ = json
