80706727832ca2ba234f4bdb56ff40ef2a406ab5
mohrahman <mohrahman558@gmail.com>
2023-11-07 17:00:45 +0000

    return characters

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -11,6 +11,9 @@ def write_file(filename="", text=""):
     Args:
         filename: text file
         text (str): text to write
+
+    Returns:
+        number of characters written
     """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
