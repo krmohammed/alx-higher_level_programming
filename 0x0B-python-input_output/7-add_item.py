9e130c31988fd64a90d5828517bbab19a34056f1
mohrahman <mohrahman558@gmail.com>
2023-11-08 00:39:35 +0000

    task 7 fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,18 +3,17 @@
 adds arguments to a Python List
 """
 import sys
-import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = 'add_item.json'
 
-if os.path.exists(filename):
+try:
     py = load_from_json_file(filename)
     if isinstance(py, list):
         py_list = py
-else:
+except FileNotFoundError:
     py_list = []
 
 py_list.extend(sys.argv[1:])
