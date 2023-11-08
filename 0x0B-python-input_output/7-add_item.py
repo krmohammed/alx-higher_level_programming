1854c9dd806b89a3f30d95e4933a86f6b37c1bdd
mohrahman <mohrahman558@gmail.com>
2023-11-08 00:30:26 +0000

    task 7 fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,21 +3,19 @@
 adds arguments to a Python List
 """
 import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def main():
-    filename = 'add_item.json'
+filename = 'add_item.json'
+
+if os.path.exists(filename):
+    py_list = load_from_json_file(filename)
+else:
     py_list = []
 
-    for i in range(1, len(sys.argv)):
-        py_list.append(sys.argv[i])
+for i in range(1, len(sys.argv)):
+    py_list.append(sys.argv[i])
 
-    save_to_json_file(py_list, filename)
-    a = load_from_json_file(filename)
-    save_to_json_file(py_list, filename)
-
-
-if __name__ == "__main__":
-    main()
+save_to_json_file(py_list, filename)
