eca86b9fd54c44287db2c8d4915f5cc8df6b4a5c
mohrahman <mohrahman558@gmail.com>
2023-11-12 11:34:43 +0000

    rectangle instance

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+"""
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """
+        """
+        return self.__height * self.__width
+
+    def display(self):
+        """
+        """
+        for i in range(self.__y):
+            print()
+        for j in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+
+    def update(self, *args, **kwargs):
+        """
+        """
+        if len(args) > 0:
+            props = ['id', 'width', 'height', 'x', 'y']
+            for ind, k in enumerate(args):
+                setattr(self, props[ind], k)
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
+    def to_dictionary(self):
+        """
+        """
+        return {
+                'id': self.id, 'width': self.width,
+                'height': self.height, 'x': self.x,
+                'y': self.y
+                }
