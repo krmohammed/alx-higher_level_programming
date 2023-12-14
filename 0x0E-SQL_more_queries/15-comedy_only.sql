b8e30102b2a5275585a2b61c46eba2f975426800
mohrahman <mohrahman558@gmail.com>
2023-12-14 03:28:14 +0000

    comedy shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,13 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows
+
+SELECT
+	shows.title as title
+FROM
+	tv_shows shows
+		JOIN
+	tv_show_genres s_gen ON shows.id = s_gen.show_id
+		JOIN
+	tv_genres genre ON s_gen.genre_id = genre.id
+WHERE
+	genre.name = 'Comedy'
+ORDER BY title ASC;
