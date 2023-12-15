105696750eb0257a4dbbf84d82706f40f43fa058
mohrahman <mohrahman558@gmail.com>
2023-12-15 00:00:38 +0000

    best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,12 +1,14 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating
 
 SELECT
-	genres.name, SUM(ratings.rate) AS rating
+    genre.name, SUM(show_rate.rate) AS ratings
 FROM
-	tv_shows shows
-		JOIN
-	tv_show_ratings ratings ON ratings.show_id = shows.id
-		JOIN
-	tv_genres genres ON genres.id = shows.id
-GROUP BY genres.name
-ORDER BY rating DESC;
+    tv_shows shows
+        JOIN
+    tv_show_ratings show_rate ON show_rate.show_id = shows.id
+        JOIN
+    tv_show_genres show_gen ON show_gen.show_id = shows.id
+        JOIN
+    tv_genres genre ON genre.id = show_gen.genre_id
+GROUP BY  genre.name
+ORDER BY ratings DESC;
