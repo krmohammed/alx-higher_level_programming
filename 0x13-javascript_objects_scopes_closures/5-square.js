9ced83b689d1961ddf2e39de7c12e9cf4790b46d
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2024-01-09 09:49:57 -0800

    changing filename

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,37 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    let areaStr = '';
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        areaStr += 'X';
-      }
-      if (this.height - i > 1) {
-        areaStr += '\n';
-      }
-    }
-    console.log(areaStr);
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
