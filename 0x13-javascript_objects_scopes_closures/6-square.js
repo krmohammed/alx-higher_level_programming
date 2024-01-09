bd0f49ddb65e6593c623e1539c4306fe8fba5560
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-01-09 19:44:10 +0000

    semistandard compliant

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Square = require('./5-square');
+const Squa = require('./5-square');
 
-module.exports = class Square extends Square {
+module.exports = class Square extends Squa {
   charPrint (c) {
     let areaStr = '';
     if (!c) c = 'X';
