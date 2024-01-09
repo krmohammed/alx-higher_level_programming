d20adffb7ad1471346c2ee7875c50ee079e3cb3f
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-01-09 17:57:55 +0000

    task 6, charPrint method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      let areaStr = '';
+      for (let i = 0; i < this.size; i++) {
+        for (let j = 0; j < this.size; j++) {
+	  areaStr += c;
+	}
+        if (this.size - i > 1) {
+	  areaStr += '\n';
+	}
+      }
+      console.log(areaStr);
+    } else {
+    	super().print();
+    }
+  }
+};
