4ef19fa0ff035f40537c444902c7d67a69adc567
Khalilu Rahman Mohmmed <mohrahman558@gmail.com>
2024-01-09 17:52:19 -0800

    refactor

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,20 +2,11 @@
 
 const fs = require('fs');
 
-function concatFiles(file1, file2, destination) {
-  try {
-    const dataFile1 = fs.readFileSync(file1, 'utf-8');
-    const dataFile2 = fs.readFileSync(file2, 'utf-8');
-    const concatenatedData = `${dataFile1}${dataFile2}`;
-
-    fs.writeFileSync(destination, concatenatedData);
-
-  } catch (error) {
-    throw error;
-  }
+function concatFiles (file1, file2, destination) {
+  const dataFile1 = fs.readFileSync(file1, 'utf-8');
+  const dataFile2 = fs.readFileSync(file2, 'utf-8');
+  const concatenatedData = `${dataFile1}${dataFile2}`;
+  fs.writeFileSync(destination, concatenatedData);
 }
-
 const args = process.argv;
-
 concatFiles(args[2], args[3], args[4]);
-
