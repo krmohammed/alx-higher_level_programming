00a05479e424e8b05a7ba804b385c66eb8b1c39f
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-01-15 01:23:38 +0000

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cursor = conn.cursor()
+    cursor.execute(
+            "SELECT * FROM states \
+                    WHERE name LIKE BINARY {} ORDER BY id ASC".format(sys.argv[4])
+            )
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
