b8518efbfa82f3474ab9f9c7cd23a3b89164237c
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-01-15 04:02:13 +0000

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+changes the name of a State object from the database
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).filter(State.id == 2).update(
+            {State.name: "New Mexico"})
+    session.commit()
