d975b4791a78deecb977935652dd58f99d3b0daa
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-02-12 13:30:52 +0000

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const num = Number(process.argv[2]);
+
+function factorial (n) {
+  if (!n) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const facto = factorial(num);
+console.log(facto);
