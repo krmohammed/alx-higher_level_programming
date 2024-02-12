7ee51ca8c74bd5bfe650fa1f264bde4f69f43570
Mohammed Khalilu Rahman <mohrahman558@gmail.com>
2024-02-12 06:08:29 -0800

    task fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const args = process.argv;
 
-if (len(args) == 3) {
+if (args.length === 3) {
   console.log('Argument found');
-} else if (len(args) > 3){
+} else if (args.length > 3) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
