3c988638714b828c6237059c38e2245ac4173ab3
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-02-12 13:32:18 +0000

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const temp = [];
+
+if (args.length <= 3) {
+  console.log(0);
+} else {
+  for (let i = 2, j = 0; i < args.length; i++) {
+    temp[j] = Number(args[i]);
+    j++;
+  }
+  temp.sort((a, b) => b - a);
+  console.log(temp[1]);
+}
