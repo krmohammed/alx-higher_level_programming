431357f9fda2c6ee8897acbee17f85a4674aa0aa
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-02-15 09:49:09 +0000

    removed orm

--- 0x0F-python-object_relational_mapping/prac/insert_students.sql
+++
@@ -1,5 +0,0 @@
-USE mqi
-INSERT INTO students (first_name, other_names, programme, gender, hall)
-VALUES ("Umar Farouq", "Imoro", "BSc. Computer Engineering", "Male", "Unity Hall"),
-        ("Yahya", "Muhyedeen", "BSc. Quantity Surveying", "Male", "Independence Hall"),
-        ("Saleema", "Muhammad", "BSc. Linguistics", "Female", "Republic Hall")
--- 0x0F-python-object_relational_mapping/prac/insert_trial.sql
+++
@@ -1,3 +0,0 @@
-USE mqi;
-INSERT INTO trial (name, day)
-VALUES ('khalil', '2008-02-04'), ('kha', '2018-02-09');
