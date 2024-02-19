b11f055ea4fe36a806acbb88b08339d195975c6b
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-02-19 00:06:15 -0800

    pycodestyle formatting

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,12 +2,14 @@
 """ lists all State objects """
 from sqlalchemy import create_engine
 from model_state import Base, State
-import sys
+from sys import argv
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
