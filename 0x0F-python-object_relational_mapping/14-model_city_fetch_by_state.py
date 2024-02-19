94a6f30a017ac7cb0e916fec897160810b715e33
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-02-19 00:14:40 -0800

    pycodestyle formatting

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,8 +8,10 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for city, state in session.query(City, State).filter(State.id == City.state_id).order_by(City.id).all():
+    for city, state in session.query(City, State).filter(
+            State.id == City.state_id).order_by(City.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
