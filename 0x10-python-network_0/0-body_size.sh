ea3fdea85cd1e73871b32e6ad02ec535bb1382d3
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-02 12:25:15 +0000

    hot fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-response=$(curl -s -o out.txt -w "%{size_download}" "$1")
-echo "$response"
+#takes in a URL and displays the size of the body of the response
+echo $(curl -s -o out.txt -w "%{size_download}" "$1")
