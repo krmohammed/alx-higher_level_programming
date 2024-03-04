dcbc3d40d8eefa4f8b85bbb0d47e34c7b3ecf121
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-04 12:21:23 +0000

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a GET request and displays the response body
-#curl -s -o out.txt -w "%{http_code}" "$1"; []
-response=$(curl -s -o temp_response.txt -w "%{http_code}" "$1"); http_code=$(head -n1 temp_response.txt); [ "$http_code" -eq 200 ] && sed '$d' temp_response.txt; rm temp_response.txt
+# displays the body of the response (200 status code only)
+curl -s "$1"
