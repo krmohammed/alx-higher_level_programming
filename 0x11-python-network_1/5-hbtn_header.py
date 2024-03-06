a90b7c85b1dbe636177ddaa10ff2dc62bfd9b060
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-06 17:53:21 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""displays the value of the X-Request-Id header"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    resp = requests.get(sys.argv[1])
+    print(resp.headers["X-Request-Id"])
