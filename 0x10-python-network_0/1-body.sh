ed770e7e540d58b78bd70d06538f326a3a7d7672
Mohammed Khalilu Rahman <mohrahman558@gmail.com>
2024-03-05 16:20:51 -0800

    task 0 fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request and displays the response body
-response=$(curl -s -o out.txt -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1"
+response=$(curl -s -o out.txt -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s -L "$1"
