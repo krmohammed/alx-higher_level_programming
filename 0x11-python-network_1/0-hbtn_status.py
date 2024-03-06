ab8d1f854b61d4917d225e99259cb7811568ad0f
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-06 16:38:49 +0000

    task 0 done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ fetches from a url """
-import urllib
+import urllib.request
 
 
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
     res = resp.read()
-print(res)
+
+print("Body response:")
+print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
+          type(res), res, res.decode("utf-8")))
