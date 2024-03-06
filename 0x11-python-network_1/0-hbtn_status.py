d4ae4e96203d2925a03ec11741266accd6961687
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-06 16:52:22 +0000

    exec

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
 import urllib.request
 
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
-    res = resp.read()
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+        res = resp.read()
 
-print("Body response:")
-print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
-          type(res), res, res.decode("utf-8")))
+    print("Body response:")
+    print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
+              type(res), res, res.decode("utf-8")))
