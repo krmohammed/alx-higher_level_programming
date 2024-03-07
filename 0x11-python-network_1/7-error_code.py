59bfc29313cf98165607e8d698f0aac6ea73ca55
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-07 10:39:11 +0000

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,5 +6,5 @@ import sys
 
 if __name__ == "__main__":
     resp = requests.get(sys.argv[1])
-    if resp.status_code > 400:
+    if resp.status_code >= 400:
         print("Error code: {}".format(resp.status_code))
