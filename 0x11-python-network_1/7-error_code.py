5cc2d5d040137e92e00f41e7cf1c3fd89e06559d
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-07 10:29:47 +0000

    task 7;

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""diplays the body of the response"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    resp = request.get(sys.argv[1])
+    if resp.status_code > 400:
+        print("Error code: {}".format(resp.status_code))
