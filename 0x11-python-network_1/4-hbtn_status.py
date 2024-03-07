9707a43271c1cf5de38f03100e653bf461915fa7
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-07 18:12:45 +0000

    task 4 review

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,6 @@ import requests
 
 if __name__ == "__main__":
     resp = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response\n\t- type: {}\n\t- content: {}".format(
+    print("Body response")
+    print("\t- type: {}\n\t- content: {}".format(
             type(resp), resp.text))
