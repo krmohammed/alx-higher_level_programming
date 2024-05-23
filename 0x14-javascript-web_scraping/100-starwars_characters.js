4c3ca5dd5b8ae89b4e8dd68daed3fc0ed074b989
Mohammed Khalilu Rahman <mohrahman558@gmail.com>
2024-05-22 21:26:25 -0700

    task 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/${id}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  if (response.statusCode === 200) {
+    const charUrls = JSON.parse(body).characters;
+
+    charUrls.forEach(charUrl => {
+      request.get(charUrl, (charError, charResp, charBody) => {
+        if (charError) {
+          console.log(charError);
+          return;
+        }
+
+        if (charResp.statusCode === 200) {
+          const character = JSON.parse(charBody);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
