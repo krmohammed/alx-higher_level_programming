9c602259ff1914d083456ecc26f9e23a0cedecb1
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-05-29 14:14:04 +0000

    task 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+$('div#toggle_header').click(function () {
+  if ($('header').hasClass('green')) {
+    $('header').removeClass('green').addClass('red')
+  } else {
+    $('header').toggleClass('red').addClass('green')
+  }
+})
