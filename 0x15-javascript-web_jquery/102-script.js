435d32ef5805284e5fd632b586bf4a1caa79723a
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-05-29 18:51:49 +0000

    hot fix

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -3,10 +3,9 @@
 $(document).ready(function () {
   $('input#btn_translate').click(function () {
     const a = $('input#language_code').val();
-    let myUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${a}`;
     $.ajax({
       type: 'GET',
-      url: myUrl,
+      url: 'https://www.fourtonfish.com/hellosalut/?lang=' + a,
       success: function (response) {
         $('div#hello').text(response.hello);
       }
